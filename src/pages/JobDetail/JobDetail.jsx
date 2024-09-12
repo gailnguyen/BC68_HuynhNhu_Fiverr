a83b4a5761967c17e68247480b432c9d4720b49b
gailnguyen <poshun.cass@gmail.com>
2024-09-13 01:23:17 +0700

    update

--- src/pages/JobDetail/JobDetail.jsx
+++ src/pages/JobDetail/JobDetail.jsx
@@ -7,6 +7,8 @@ import { binhLuanService } from "../../services/binhLuan.service";
 import InputCustom from "../../components/InputCustom/InputCustom";
 import { useFormik } from "formik";
 import { useSelector } from "react-redux";
+import "./jobDetail.scss";
+
 const items = [
   {
     key: "1",
@@ -119,7 +121,7 @@ const JobDetail = () => {
       return <div className="mt-36">...loading</div>;
     }
     return (
-      <div>
+      <div className="job_detail space-y-10">
         {/* breadcrumbs */}
         <ul className="breadcrumbs flex items-center">
           <li>
@@ -131,55 +133,233 @@ const JobDetail = () => {
             </a>
           </li>
           <li>
-            <span className="divider mx-3">/</span>
+            <span className="divider mx-3 text-gray-400">/</span>
             <span>{jobDetail.tenLoaiCongViec}</span>
           </li>
           <li>
-            <span className="divider mx-3">/</span>
+            <span className="divider mx-3 text-gray-400">/</span>
             <span>{jobDetail.tenNhomChiTietLoai}</span>
           </li>
           <li>
-            <span className="divider mx-3">/</span>
+            <span className="divider mx-3 text-gray-400">/</span>
             <span>{jobDetail.tenChiTietLoai}</span>
           </li>
         </ul>
 
         {/* title */}
-        <div className="grid grid-cols-3 gap-20">
-          <div className="col-span-2">
+        <div className="grid grid-cols-3 gap-16 px-16">
+          <div className="col-span-2 space-y-5">
             {/* job title */}
-            <h2>{jobDetail.congViec.tenCongViec}</h2>
-
-            {/* đánh giá */}
-            <div className="space-x-2">
-              <i class="fa-solid fa-star"></i>
-              <span>{jobDetail.congViec.saoCongViec}.0</span>
-              <span>({jobDetail.congViec.danhGia})</span>
-            </div>
+            <h3>{jobDetail.congViec.tenCongViec}</h3>
 
             {/* author */}
-            <div className="user flex">
-              <img src={jobDetail.avatar} width="80px" alt="" />
+            <div className="user flex items-center gap-8">
+              <img
+                src={jobDetail.avatar}
+                width="80px"
+                className="rounded-full"
+                alt=""
+              />
 
-              <div>
-                <h4>{jobDetail.tenNguoiTao}</h4>
-                <button className="py-2 px-5 rounded-md border">
-                  Contact me
-                </button>
+              <div className="space-y-3">
+                <h4 className="uppercase font-bold">{jobDetail.tenNguoiTao}</h4>
+                <div className="space-x-2">
+                  <i class="fa-solid fa-star"></i>
+                  <span>{jobDetail.congViec.saoCongViec}.0</span>
+                  <span>({jobDetail.congViec.danhGia})</span>
+                </div>
               </div>
             </div>
 
             {/* job image */}
-            <img src={jobDetail.congViec.hinhAnh} alt="" />
+            <img src={jobDetail.congViec.hinhAnh} width="100%" alt="" />
 
             {/* about gig */}
-            <div>
+            <div className="space-y-3">
               <h4>About this Gig:</h4>
               <p>{jobDetail.congViec.moTaNgan}</p>
               <span>Read More</span>
             </div>
 
             {/* Q&A */}
+            <div id="accordion-collapse" data-accordion="collapse">
+              <h2 id="accordion-collapse-heading-1">
+                <button
+                  type="button"
+                  className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
+                  data-accordion-target="#accordion-collapse-body-1"
+                  aria-expanded="true"
+                  aria-controls="accordion-collapse-body-1"
+                >
+                  <span>What is Flowbite?</span>
+                  <svg
+                    data-accordion-icon
+                    className="w-3 h-3 rotate-180 shrink-0"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 10 6"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M9 5 5 1 1 5"
+                    />
+                  </svg>
+                </button>
+              </h2>
+              <div
+                id="accordion-collapse-body-1"
+                className="hidden"
+                aria-labelledby="accordion-collapse-heading-1"
+              >
+                <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+                  <p className="mb-2 text-gray-500 dark:text-gray-400">
+                    Flowbite is an open-source library of interactive components
+                    built on top of Tailwind CSS including buttons, dropdowns,
+                    modals, navbars, and more.
+                  </p>
+                  <p className="text-gray-500 dark:text-gray-400">
+                    Check out this guide to learn how to{"{"}" "{"}"}
+                    <a
+                      href="/docs/getting-started/introduction/"
+                      className="text-blue-600 dark:text-blue-500 hover:underline"
+                    >
+                      get started
+                    </a>
+                    {"{"}" "{"}"}
+                    and start developing websites even faster with components on
+                    top of Tailwind CSS.
+                  </p>
+                </div>
+              </div>
+              <h2 id="accordion-collapse-heading-2">
+                <button
+                  type="button"
+                  className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
+                  data-accordion-target="#accordion-collapse-body-2"
+                  aria-expanded="false"
+                  aria-controls="accordion-collapse-body-2"
+                >
+                  <span>Is there a Figma file available?</span>
+                  <svg
+                    data-accordion-icon
+                    className="w-3 h-3 rotate-180 shrink-0"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 10 6"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M9 5 5 1 1 5"
+                    />
+                  </svg>
+                </button>
+              </h2>
+              <div
+                id="accordion-collapse-body-2"
+                className="hidden"
+                aria-labelledby="accordion-collapse-heading-2"
+              >
+                <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700">
+                  <p className="mb-2 text-gray-500 dark:text-gray-400">
+                    Flowbite is first conceptualized and designed using the
+                    Figma software so everything you see in the library has a
+                    design equivalent in our Figma file.
+                  </p>
+                  <p className="text-gray-500 dark:text-gray-400">
+                    Check out the{"{"}" "{"}"}
+                    <a
+                      href="https://flowbite.com/figma/"
+                      className="text-blue-600 dark:text-blue-500 hover:underline"
+                    >
+                      Figma design system
+                    </a>
+                    {"{"}" "{"}"}
+                    based on the utility classes from Tailwind CSS and
+                    components from Flowbite.
+                  </p>
+                </div>
+              </div>
+              <h2 id="accordion-collapse-heading-3">
+                <button
+                  type="button"
+                  className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
+                  data-accordion-target="#accordion-collapse-body-3"
+                  aria-expanded="false"
+                  aria-controls="accordion-collapse-body-3"
+                >
+                  <span>
+                    What are the differences between Flowbite and Tailwind UI?
+                  </span>
+                  <svg
+                    data-accordion-icon
+                    className="w-3 h-3 rotate-180 shrink-0"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 10 6"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M9 5 5 1 1 5"
+                    />
+                  </svg>
+                </button>
+              </h2>
+              <div
+                id="accordion-collapse-body-3"
+                className="hidden"
+                aria-labelledby="accordion-collapse-heading-3"
+              >
+                <div className="p-5 border border-t-0 border-gray-200 dark:border-gray-700">
+                  <p className="mb-2 text-gray-500 dark:text-gray-400">
+                    The main difference is that the core components from
+                    Flowbite are open source under the MIT license, whereas
+                    Tailwind UI is a paid product. Another difference is that
+                    Flowbite relies on smaller and standalone components,
+                    whereas Tailwind UI offers sections of pages.
+                  </p>
+                  <p className="mb-2 text-gray-500 dark:text-gray-400">
+                    However, we actually recommend using both Flowbite, Flowbite
+                    Pro, and even Tailwind UI as there is no technical reason
+                    stopping you from using the best of two worlds.
+                  </p>
+                  <p className="mb-2 text-gray-500 dark:text-gray-400">
+                    Learn more about these technologies:
+                  </p>
+                  <ul className="ps-5 text-gray-500 list-disc dark:text-gray-400">
+                    <li>
+                      <a
+                        href="https://flowbite.com/pro/"
+                        className="text-blue-600 dark:text-blue-500 hover:underline"
+                      >
+                        Flowbite Pro
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://tailwindui.com/"
+                        rel="nofollow"
+                        className="text-blue-600 dark:text-blue-500 hover:underline"
+                      >
+                        Tailwind UI
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
             <div>
               <ul>
                 <li>
@@ -301,7 +481,7 @@ const JobDetail = () => {
             {/* bình luận */}
             {comment.map((item) => {
               return (
-                <div className="w-full border border-gray-500 rounded-lg space-y-5 my-10 p-10">
+                <div className="w-full border border-gray-200 rounded-lg space-y-5 my-5 p-3">
                   <div className="flex py-3 border-b-2">
                     <img
                       src={item.avatar}
